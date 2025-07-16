7f49d7348835d504a8db32fd3a88dc6310362c3c
Maneesha Geethanga <118714345+ManeeshaGee@users.noreply.github.com>
2025-07-16 05:47:16 +0530

    Update Modal.jsx

--- Frontend/src/components/ui/Modal.jsx
+++ Frontend/src/components/ui/Modal.jsx
@@ -46,7 +46,7 @@ export const Modal = ({
     : "relative w-full rounded-3xl bg-white dark:bg-gray-900";
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center overflow-y-auto modal z-99999">
+    <div className="fixed inset-0 flex items-center justify-center overflow-y-auto modal z-50">
       {!isFullscreen && (
         <div
           className="fixed inset-0 h-full w-full bg-gray-400/50 backdrop-blur-[32px]"
@@ -61,7 +61,7 @@ export const Modal = ({
         {showCloseButton && (
           <button
             onClick={onClose}
-            className="absolute right-3 top-3 z-999 flex h-9.5 w-9.5 items-center justify-center rounded-full bg-gray-100 text-gray-400 transition-colors hover:bg-gray-200 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white sm:right-6 sm:top-6 sm:h-11 sm:w-11"
+            className="absolute right-3 top-3 z-10 flex h-9.5 w-9.5 items-center justify-center rounded-full bg-gray-100 text-gray-400 transition-colors hover:bg-gray-200 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white sm:right-6 sm:top-6 sm:h-11 sm:w-11"
           >
             <svg
               width="24"
@@ -92,10 +92,4 @@ Modal.propTypes = {
   className: PropTypes.string,
   showCloseButton: PropTypes.bool,
   isFullscreen: PropTypes.bool,
-};
-
-Modal.defaultProps = {
-  className: "",
-  showCloseButton: true,
-  isFullscreen: false,
 };
